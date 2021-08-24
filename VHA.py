14e8194363e94019cc03f5b1cee4b996e4c49dd9
Kaushik Roy <kaushikroy@Kaushiks-MacBook-Pro.local>
2021-08-23 22:48:39 -0400

    updated depression KG

--- VHA.py
+++ VHA.py
@@ -24,6 +24,14 @@ class VHA(object):
         self.decode_instructions = decode_instructions
         self.nlg_module = nlg_module
 
+    def set_guidelines(self,
+                       guidelines):
+        '''sets the list of guidelines
+           KG init
+        '''
+
+        self.guidelines = guidelines
+
 
     def add_guideline(self,
                       guideline):
@@ -66,5 +74,45 @@ class VHA(object):
 def main():
 
     my_vha = VHA()
-        
+    my_vha.set_guidelines([depression(X):-significant(X,S),
+                           significant(X,S):-depSymptom(X,S);freq(S,"high")])
+    
+                           depSymptom(X,"feelings_of_sadness"),
+                           depSymptom(X,"tearfulness"),
+                           depSymptom(X,"emptiness"),
+                           depSymptom(X,"hopelessness"),
+                           depSymptom(X,"angry_outbursts"),
+                           depSymptom(X,"irritability"),
+                           depSymptom(X,"frustration"),
+                           depSymptom(X,"loss_of_interest"),
+                           depSymptom(X,"loss_of_pleasure"),
+                           depSymptom(X,"loss_of_hobbies"),
+                           depSymptom(X,"sleep_disturbances"),
+                           depSymptom(X,"insomnia"),
+                           depSymptom(X,"sleeping_too_much"),
+                           depSymptom(X,"tiredness"),
+                           depSymptom(X,"lack_of_energy"),
+                           depSymptom(X,"reduced_appetite"),
+                           depSymptom(X,"weight_loss"),
+                           depSymptom(X,"increased_cravings"),
+                           depSymptom(X,"weight_gain"),
+                           depSymptom(X,"anxiety"),
+                           depSymptom(X,"agitation"),
+                           depSymptom(X,"restlessness"),
+                           depSymptom(X,"slowed_thinking"),
+                           depSymptom(X,"slow_speaking"),
+                           depSymptom(X,"slow_body_movements"),
+                           depSymptom(X,"feelings_of_worthlessness"),
+                           depSymptom(X,"feelings_of_guilt"),
+                           depSymptom(X,"feelings_of_failure"),
+                           depSymptom(X,"feelings_of_blame"),
+                           depSymptom(X,"trouble_thinking"),
+                           depSymptom(X,"trouble_concentrating"),
+                           depSymptom(X,"trouble_with_decisions"),
+                           depSymptom(X,"trouble_remembering_things"),
+                           depSymptom(X,"frequent_thoughts_of_death"),
+                           depSymptom(X,"suicidal_thoughts"),
+                           depSymptom(X,"suicide_attempts"),
+                           depSymptom(X,"suicide"),
+                           depSymptom(X,"unexplained_problems")])
 
